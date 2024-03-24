4f8b6005c2b8f151fa540232e8f33aac07663ffe
Student <daisychepkiruih@gmail.com>
2024-03-24 18:33:45 +0300

    add changes

--- array2.js
+++ array2.js
@@ -6,36 +6,29 @@ let arr2=[true,'green','where',12,56]
 let last=arr2.pop()
 console.log({arr2})
 
-//Write a JS program that will join the following array elements 
+ 
 let myPet=['Cow','Birds','Snake','Dog'];
-let joinString= myPet.join(',');
-console.log({joinString});
-//Write a JS script to sort the following array itemsvar 
+console.log(myPet.join(","));
+
 let arr3=[-5,9,5,3,2,-3,6,8,4,1];
-let sortedString=arr3.sort();
-console.log({sortedString})
-//Write a program to remove duplicates from the following array.
+arr3.sort();
+console.log({arr3})
+
 let arr4=['apple','mango','apple','orange','mango','mango'];
-let uniqueArr =[...newSet(arr)];
-let duplicateArr=arr.filter((item,index)=  arr.indexOf(item)!==index);
-console.log({uniqueArr});
-console.log({duplicateArr});
-//Write a JS script to search for the following word in the array
+function removeDuplicate(arr4){
+return[...new Set(arr4)];
+}
+console.log(removeDuplicate(arr4))
+
+
+console.log({removeDuplicate});
+
 let arr5=["the","way","x",4];
-let found =false;
-for(let i=0;i<arr5.length;i++){
-    if(arr5[i]==="way"){
-        console.log("way");
-        found= true
-        break;
+let f="way"
+console.log(arr5.includes(f))
 
-    }
 
-}
-if(! found){
-    console.log("the search word was not found");
-}
-//Write a JS script to sort the following string:let word = "sevink"
 let word="sevink";
-let sortedWord= word.sort();
-console.log({sortedWord})
+let sortedWord =word.split(',').sort().join();
+console.log(sortedWord)
+
